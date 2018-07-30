347c004f58742eb7e6731288bf7ff6c61dac38c4
Delong Meng <dlmeng@yitu-inc.intra>
2018-07-30 21:39:39 +0800

    change shfl_down to shl_down_sync

--- include/contrib/moderngpu/include/device/intrinsics.cuh
+++ include/contrib/moderngpu/include/device/intrinsics.cuh
@@ -112,7 +112,7 @@ __device__ __forceinline__ float shfl_up(float var,
 	unsigned int delta, int width = 32) {
 
 #if __CUDA_ARCH__ >= 300
-	var = __shfl_up(var, delta, width);
+	var = __shfl_up_sync(var, delta, width);
 #endif
 	return var;
 }
@@ -122,8 +122,8 @@ __device__ __forceinline__ double shfl_up(double var,
 
 #if __CUDA_ARCH__ >= 300
 	int2 p = mgpu::double_as_int2(var);
-	p.x = __shfl_up(p.x, delta, width);
-	p.y = __shfl_up(p.y, delta, width);
+	p.x = __shfl_up_sync(p.x, delta, width);
+	p.y = __shfl_up_sync(p.y, delta, width);
 	var = mgpu::int2_as_double(p);
 #endif
 
